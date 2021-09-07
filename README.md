d9d056fc49dee84903292a91c330b2733080a179
dymafr <erwan1309@hotmail.com>
2021-09-07 13:11:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-c6-l3
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-c6-l3)
---
+++ src/shared/surligner.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({ selector: '[monSurlignage]' })
+export class SurlignerDirective {
+
+  constructor(private el: ElementRef) {}
+  
+  @HostListener('mouseenter') onMouseEnter() {
+    this.surligner('red');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.surligner(null);
+  }
+
+  private surligner(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
+
+}
